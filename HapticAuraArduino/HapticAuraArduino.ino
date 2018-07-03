a3c79986fe9f8e1420e63cb6ce6324774f593d71
Toby Wu <towu1344@colorado.edu>
2018-07-03 16:13:55 -0600

    Update

--- HapticAuraArduino/HapticAuraArduino.ino
+++ HapticAuraArduino/HapticAuraArduino.ino
@@ -1,29 +1,85 @@
-// Define main variables
-int ledPin = 13;
-// Slowing down the process
-//unsigned long millisCounter = 0;
-//int interval = 10;
-// activate the pins I want to use on my board
-int myPin = 13;
+int sValve_1 = 0;
+int sValve_2 = 1;
+int sValve_3 = 2;
+int sValve_4 = 3;
+int sValve_5 = 4;
+int sValve_6 = 5;
+int sValve_7 = 6;
+int sValve_8 = 7;
 
 
 void setup() {
-// Activate all Digital pins on my board
-  for (int i = 0; i < 5; i++) {
-    pinMode(myPin, OUTPUT);
-  }
+
+  pinMode(sValve_1, OUTPUT);
+  pinMode(sValve_2, OUTPUT);
+  pinMode(sValve_3, OUTPUT);
+  pinMode(sValve_4, OUTPUT);
+  pinMode(sValve_5, OUTPUT);
+  pinMode(sValve_6, OUTPUT);
+  pinMode(sValve_7, OUTPUT);
+  pinMode(sValve_8, OUTPUT);
+
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available()) {
-    char currentRead = Serial.read();
+    int currentRead = Serial.read();
   
-    if(currentRead == 'H') {
-      digitalWrite(ledPin, HIGH);
+    if(currentRead == "AH") {
+      digitalWrite(sValve_1, HIGH);
     }
-    if(currentRead == 'L') {
-      digitalWrite(ledPin, LOW);
+    if(currentRead == "AL") {
+      digitalWrite(sValve_1, LOW);
+    }
+
+    if(currentRead == "BH") {
+      digitalWrite(sValve_2, HIGH);
+    }
+    if(currentRead == "BL") {
+      digitalWrite(sValve_2, LOW);
+    }
+
+    if(currentRead == "CH") {
+      digitalWrite(sValve_3, HIGH);
+    }
+    if(currentRead == "CL") {
+      digitalWrite(sValve_3, LOW);
+    }
+
+    if(currentRead == "DH") {
+      digitalWrite(sValve_4, HIGH);
+    }
+    if(currentRead == "DL") {
+      digitalWrite(sValve_4, LOW);
+    }
+
+    if(currentRead == "EH") {
+      digitalWrite(sValve_5, HIGH);
+    }
+    if(currentRead == "EL") {
+      digitalWrite(sValve_5, LOW);
+    }
+
+    if(currentRead == "FH") {
+      digitalWrite(sValve_6, HIGH);
+    }
+    if(currentRead == "FL") {
+      digitalWrite(sValve_6, LOW);
+    }
+
+    if(currentRead == "GH") {
+      digitalWrite(sValve_7, HIGH);
+    }
+    if(currentRead == "GL") {
+      digitalWrite(sValve_7, LOW);
+    }
+
+    if(currentRead == "HH") {
+      digitalWrite(sValve_8, HIGH);
+    }
+    if(currentRead == "HL") {
+      digitalWrite(sValve_8, LOW);
     }
   }
 }
