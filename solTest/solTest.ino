0a83ce5eca00e3ee07a56e7a947d696a0800daaa
Toby Wu <towu1344@colorado.edu>
2018-07-03 16:16:37 -0600

    Solenoid test

---
+++ solTest/solTest.ino
@@ -0,0 +1,155 @@
+int sValve_1 = 0;
+int sValve_2 = 1;
+int sValve_3 = 2;
+int sValve_4 = 3;
+int sValve_5 = 4;
+int sValve_6 = 5;
+int sValve_7 = 6;
+int sValve_8 = 7;
+int sValve_9 = 8;
+int sValve_10 = 9;
+int sValve_11 = 10;
+int sValve_12 = 11;
+int sValve_13 = 12;
+int sValve_14 = 13;
+
+int delayMill = 1000;
+
+void setup() {
+
+  Serial.begin(9600);
+
+  pinMode(sValve_1, OUTPUT);
+  pinMode(sValve_2, OUTPUT);
+  pinMode(sValve_3, OUTPUT);
+  pinMode(sValve_4, OUTPUT);
+  pinMode(sValve_5, OUTPUT);
+  pinMode(sValve_6, OUTPUT);
+  pinMode(sValve_7, OUTPUT);
+  pinMode(sValve_8, OUTPUT);
+  pinMode(sValve_9, OUTPUT);
+  pinMode(sValve_10, OUTPUT);
+  pinMode(sValve_11, OUTPUT);
+  pinMode(sValve_12, OUTPUT);
+  pinMode(sValve_13, OUTPUT);
+  pinMode(sValve_14, OUTPUT);
+
+ 
+  
+}
+  
+
+void loop() {
+
+//    digitalWrite(sValve_1, LOW);
+//    Serial.print("1 on");
+//    Serial.println();
+//    digitalWrite(sValve_1, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_1, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_2, LOW);
+//    Serial.print("2 on");
+//    Serial.println();
+//    digitalWrite(sValve_2, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_2, LOW);
+//    delay(delayMill);
+
+    digitalWrite(sValve_3, LOW);
+    Serial.print("3 on");
+    Serial.println();
+    digitalWrite(sValve_3, HIGH);
+    delay(delayMill);
+    digitalWrite(sValve_3, LOW);
+    delay(delayMill);
+
+//    digitalWrite(sValve_4, LOW);
+//    Serial.print("4 on");
+//    Serial.println();
+//    digitalWrite(sValve_4, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_4, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_5, LOW);
+//    Serial.print("5 on");
+//    Serial.println();
+//    digitalWrite(sValve_5, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_5, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_6, LOW);
+//    Serial.print("6 on");
+//    Serial.println();
+//    digitalWrite(sValve_6, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_6, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_7, LOW);
+//    Serial.print("7 on");
+//    Serial.println();
+//    digitalWrite(sValve_7, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_7, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_8, LOW);
+//    Serial.print("8 on");
+//    Serial.println();
+//    digitalWrite(sValve_8, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_8, LOW);
+//    delay(delayMill);
+
+//    digitalWrite(sValve_9, LOW);
+//    Serial.print("9 on");
+//    Serial.println();
+//    digitalWrite(sValve_9, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_9, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_10, LOW);
+//    Serial.print("10 on");
+//    Serial.println();
+//    digitalWrite(sValve_10, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_10, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_11, LOW);
+//    Serial.print("11 on");
+//    Serial.println();
+//    digitalWrite(sValve_11, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_11, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_12, LOW);
+//    Serial.print("12 on");
+//    Serial.println();
+//    digitalWrite(sValve_12, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_12, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_13, LOW);
+//    Serial.print("13 on");
+//    Serial.println();
+//    digitalWrite(sValve_13, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_13, LOW);
+//    delay(delayMill);
+//
+//    digitalWrite(sValve_14, LOW);
+//    Serial.print("14 on");
+//    Serial.println();
+//    digitalWrite(sValve_14, HIGH);
+//    delay(delayMill);
+//    digitalWrite(sValve_14, LOW);
+//    delay(delayMill);
+}
